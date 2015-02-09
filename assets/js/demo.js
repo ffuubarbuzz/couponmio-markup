6aed560071ca2f8e741ba1bc29064556ea498874
Paul Kozlovitch <paul.kozlovitch@gmail.com>
2015-02-09 19:06:27 +0300

    added: all components from main page

---
+++ assets/js/demo.js
@@ -0,0 +1,53 @@
+$(function(){
+	$('.coupon .share').click(function(){
+		$(this).closest('.coupon').toggleClass('social-open');
+	});
+	$('.coupon-slider').each(function(){
+		var $couponListItems = $(this).find('.coupon-list li');
+		function repaint() {
+			//dirty fix for chrome not repaint bug
+			$couponListItems.find('img').hide();
+			$couponListItems.find('img').outerWidth();
+			$couponListItems.find('img').show();
+		}
+		$couponListItems.click(function(e){
+			var $couponListItem = $(this);
+			e.preventDefault();
+			$couponListItems.removeClass('current');
+			$couponListItem.addClass('current');
+			repaint();
+		});
+		$(this).find('.action.next').click(function(){
+			var $currentItem = $couponListItems.filter('.current');
+			var $firstItem = $couponListItems.filter(':first');
+			var $lastItem = $couponListItems.filter(':last');
+			$currentItem.removeClass('current');
+			if ( $currentItem.is($lastItem) ) {
+				$firstItem.addClass('current');
+			}
+			else {
+				$currentItem.next().addClass('current');
+			}
+			repaint();
+		});
+
+		$(this).find('.action.prev').click(function(){
+			var $currentItem = $couponListItems.filter('.current');
+			var $firstItem = $couponListItems.filter(':first');
+			var $lastItem = $couponListItems.filter(':last');
+			$currentItem.removeClass('current');
+			if ( $currentItem.is($firstItem) ) {
+				$lastItem.addClass('current');
+			}
+			else {
+				$currentItem.prev().addClass('current');
+			}
+			repaint();
+		});
+	});
+
+	$('.categories>ul>li').click(function(e){
+		e.preventDefault();
+		$(this).toggleClass('open')
+	});
+});
